65e4c2530e0fd473132caa2f9f0506191af3e645
The Quetapiton Dev <oamost123@gmail.com>
2025-12-18 21:04:33 -0300

    Add files via upload

--- erromadeite-terminal/demo2/main.cpp
+++ erromadeite-terminal/demo2/main.cpp
@@ -2,20 +2,37 @@
 #include <direct.h>
 #include <string>
 #include "libs/createfile.hpp"
+#include "libs/createrandom_string.hpp"
 
 using namespace std;
 
 int main() {
     string comando;
+    system("title Erromadeite terminal");
+
     while (1) {
         cout << "\n>";
-        cin >> comando;
+        getline(cin, comando);
         if (comando == "156") {
             cout << "Erromadeite is red\n";
         } else if (comando == "clear") {
-            system("clear");
+            system("cls");
         } else if (comando == "github") {
             cout << "https://github.com/theQuetapitonStudio/quetapiton-IDE/tree/main/erromadeite-terminal \n";
+        } else if (comando == "execute createkey") {
+            cout << "Your key " << randomString(30) << "\n";
+        } else if (comando == "execute createtxt") {
+            _mkdir("txt");
+            _chdir("txt");
+            createFile("Your text here \n your subtext here", "yf.txt");
+            _chdir("..");
+        } else if (comando == "execute createbat") {
+            _mkdir("batch-scripts");
+            _chdir("batch-scripts");
+            createFile("@echo off", "app.bat");
+            _chdir("..");
+        } else {
+            cout << "invalid comand\n";
         }
     }   
 }
